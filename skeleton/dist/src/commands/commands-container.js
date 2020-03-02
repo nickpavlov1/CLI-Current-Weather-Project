77f5bbd9d6ade831865e440a76ec4f51238a858f
nickpavlov <nikolaypavlov4@gmail.com>
2020-03-02 22:44:45 +0200

    delete dist

--- skeleton/dist/src/commands/commands-container.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CommandContainer {
-    constructor(help, getWeather) {
-        this.help = help;
-        this.getWeather = getWeather;
-    }
-}
-exports.CommandContainer = CommandContainer;
--- skeleton/dist/src/tools/decorators/injectable.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Injectable = () => {
-    return (target) => {
-        // for debugging and tracing injectables only
-        // console.log(`${target.name} is now injectable`);
-    };
-};
