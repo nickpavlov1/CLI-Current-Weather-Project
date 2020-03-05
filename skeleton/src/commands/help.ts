4113da9143d9e070da2c6346277af8ea1ba2fb80
nickpavlov <nikolaypavlov4@gmail.com>
2020-03-05 20:33:24 +0200

    Adding information to the help manual printer in the HelpCommand class

--- skeleton/src/commands/help.ts
+++ skeleton/src/commands/help.ts
@@ -13,7 +13,21 @@ export class HelpCommand implements ICommand {
   public async execute(): Promise<ExecutionResult> {
     this.printer.print(
       `
-      Your cli documentation here...
+      Usage: getweather <command>
+
+      where <command> is one of:
+        help, today
+
+      getweather help                 prints this manual
+
+      getweather today                generates current weather information for the given city
+        arguments:
+          --city=<string>             providing a city to check the current weather
+          --f (or --fahrenheit)       if passed converts the temperature to Fahrenheit
+          --w (or --wind)             if passed provides information for the wind speed at the given location.
+          --h (or --humidity)         if passed provides information for the level of humidity at the given location.
+        example:
+          getweather today --city=berlin --f --w --h 
       `
     );
 
